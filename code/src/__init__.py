9f00b4cd4fedd367abe59d52fa66bf2cd329b21f
Mac Gaulin <gaulinmp@gmail.com>
2025-08-13 09:38:46 -0600

    Add python module with compustat pull

---
+++ code/src/__init__.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# STDlib imports
+import os
+from pathlib import Path
+import logging
+
+# 3rd party package imports
+import numpy as np
+import pandas as pd
+from urllib.parse import quote_plus
+import sqlalchemy as sa
+from sqlalchemy.exc import DBAPIError
+try:
+    import psycopg
+    PSYCOPG = "psycopg"
+except ImportError:
+    PSYCOPG = "psycopg2"
+
+# project imports
+for pwd_path in ("~/Dropbox/Documents/Personal/config/wrds.pwd", "~/wrds.pwd"):
+    try:
+        with open(os.path.expanduser(pwd_path), "r", encoding="utf-8") as fh:
+            wrds_user, wrds_password = fh.read().strip().split("\n")
+            os.environ["WRDS_USER"] = wrds_user
+            os.environ["WRDS_PASSWORD"] = wrds_password
+    except FileNotFoundError:
+        continue
+
+# project imports
+ROOT_DIR = Path('.').absolute()
+for i in range(10):
+    if len({'code', 'data'} & set(os.listdir(ROOT_DIR))) == 2:
+        break
+    ROOT_DIR = ROOT_DIR.parent
+else:
+    raise RuntimeError('Cannot find root directory!')
+
+CODE_DIR = ROOT_DIR / "code"
+DATA_DIR = ROOT_DIR / "data"
+
+# Local logger
+_logger = logging.getLogger(__name__)
+
+
+def string_to_int(x):
+    """convert cik and gvkey to integers (ignoring nan and None)"""
+    try:
+        return int(x)
+    except ValueError:
+        return np.nan
+
+
+class wrds_connection(object):
+    db_url = "wrds-pgdata.wharton.upenn.edu"
+    db_port = 9737
+    db_table = "wrds"
+    db_conargs = {"sslmode": "require", "application_name": "win32 python 3.10.0/wrds 3.2.0"}
+
+    engine = None
+    connection = None
+
+    def __init__(self, wrds_user=None, wrds_password=None):
+        self._usr = wrds_user or os.environ.get("WRDS_USER", None)
+        self._pass = wrds_password or os.environ.get("WRDS_PASSWORD", None)
+        if self._usr is None or self._pass is None:
+            raise ValueError("Please set WRDS_USER and WRDS_PASSWORD environment variables.")
+        # [DB_FLAVOR]+[DB_PYTHON_LIBRARY]://[USERNAME]:[PASSWORD]@[DB_HOST]:[PORT]/[DB_NAME]
+        self._url = (
+            f"postgresql+{PSYCOPG}://{self._usr}:{quote_plus(self._pass)}@{self.db_url}:{self.db_port}/{self.db_table}"
+        )
+
+    def __enter__(self):
+        self.engine = sa.create_engine(self._url, isolation_level="AUTOCOMMIT", connect_args=self.db_conargs)
+        self.connection = self.engine.connect()
+        _logger.debug(f"Connected to {self.engine}")
+        return self
+
+    def __exit__(self, *args, **kwargs):
+        self.connection.close()
+        self.engine.dispose()
+        self.engine = None
+
+    def read_sql(self, sql_statement, *args, chunksize=500000, **kwargs):
+        # could also be self.engine? Maybe this doesn't work anymore?
+        if isinstance(sql_statement, str):
+            sql_statement = sa.text(sql_statement)
+            _logger.debug(f"Executing SQL: {sql_statement}")
+        try:
+            df = pd.read_sql_query(sql_statement, self.connection, *args, chunksize=chunksize, **kwargs)
+            _logger.debug(f"SQL query executed successfully, retrieved {df}.")
+            if chunksize is None:
+                return df
+            else:
+                try:
+                    _logger.debug(f"Returning {len(df)} chunks of {len(df[0])} rows.")
+                except Exception:
+                    _logger.debug(f"Returning chunks: {df}.")
+                return pd.concat(df)
+        except DBAPIError as e:
+            _logger.error(f"Error: {e}")
+            raise
---
+++ code/src/compustat.py
@@ -0,0 +1,181 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# STDlib imports
+import os
+from pathlib import Path
+import logging
+# import datetime as dt
+
+# 3rd party package imports
+import numpy as np
+import pandas as pd
+from reslib.data.cache import DataFrameCache as _DataFrameCache
+
+# project imports
+from . import wrds_connection, DATA_DIR
+
+
+# Local _logger
+_logger = logging.getLogger(__name__)
+
+
+class CompustatAnnual(_DataFrameCache):
+    """
+    Read Compustat data from cache if it exists,
+    or read from WRDS Postrgres server, and write out to
+    `interim` cache folder.
+
+    Variables: https://wrds-www.wharton.upenn.edu/data-dictionary/comp_na_daily_all/funda/
+    """
+
+    # Arguments passed to _pd.read_csv
+    file_format = "csv"
+    read_args = {"compression": None}
+    override_directory = DATA_DIR
+
+    def make_dataset(self):
+        # Build sub-queries
+        gics = """
+            SELECT gvkey AS gvkey_hgic, ggroup AS ggroup_hgic, gind AS gind_hgic,
+                    gsector AS gsector_hgic, gsubind AS gsubind_hgic,
+                    indfrom AS indfrom_hgic, indthru AS indthru_hgic
+            FROM comp.co_hgic"""
+
+        # final FUNDA query
+        funda_query = f"""
+            SELECT a.gvkey, a.tic, a.conm AS name, a.fyear, a.fyr as fiscal_year_end_month
+                , (a.datadate - ast.funda_start) AS age_days
+                , at, act, lt, lct, invt, ap, rect, recd
+                , (COALESCE(dltt, 0) + COALESCE(dlc, 0)) AS total_debt, che, re
+                , oancf, ebit, ebitda, ni, ib, pi, sale, revt, epspi, epspx
+                , xad, xrd, xsga, xint, capx, cogs, dvt
+                , emp
+                , prcc_f AS share_price, csho AS shares_outstanding
+                , GREATEST(a.prcc_f * a.csho, COALESCE(mkvalt, 0)) AS mve
+                , COALESCE(seq /* Shareholder Equity is reported SE if not missing */
+                    /* if SEQ missing, use Total Common Equity plus Preferred Stock Par Value  */
+                    ,ceq + pstk
+                    /* otherwise, Total Assets-(Total Liabilities+Minority Interest) */
+                    ,at - lt - COALESCE(mib, 0))
+                    AS seq
+                , seq AS bve
+                , CASE
+                    WHEN CAST(a.au AS INTEGER) >= 1 AND CAST(a.au AS INTEGER) <= 8 THEN 1
+                    ELSE 0
+                END AS bign
+                , CASE au
+                    WHEN '0' THEN 'Unaudited'
+                    WHEN '1' THEN 'Arthur Andersen'
+                    WHEN '2' THEN 'Arthur Young'
+                    WHEN '3' THEN 'Coopers & Lybrand'
+                    WHEN '4' THEN 'Ernst & Young'
+                    WHEN '5' THEN 'Deloitte & Touche'
+                    WHEN '6' THEN 'KPMG'
+                    WHEN '7' THEN 'PricewaterhouseCoopers'
+                    WHEN '8' THEN 'Touche Ross'
+                    WHEN '9' THEN 'Other'
+                    WHEN '10' THEN 'Altschuler, Melvoin and Glasser'
+                    WHEN '11' THEN 'BDO International'
+                    WHEN '12' THEN 'BKD LLP'
+                    WHEN '13' THEN 'Cherry Bekaert LLP'
+                    WHEN '14' THEN 'Clarkson, Gordon'
+                    WHEN '15' THEN 'CliftonLarsonAllen LLP'
+                    WHEN '16' THEN 'Crowe Horwath'
+                    WHEN '17' THEN 'Grant Thornton'
+                    WHEN '18' THEN 'CohnReznick LLP'
+                    WHEN '19' THEN 'Kenneth Leventhal'
+                    WHEN '20' THEN 'Laventhol and Horwath'
+                    WHEN '21' THEN 'RSM'
+                    WHEN '22' THEN 'Moore Stephens'
+                    WHEN '23' THEN 'Moss Adams'
+                    WHEN '24' THEN 'PKF International'
+                    WHEN '25' THEN 'Plante & Moran'
+                    WHEN '26' THEN 'EisnerAmper LLP'
+                    WHEN '27' THEN 'Spicer & Oppenheim'
+                    ELSE 'Other'
+                END AS auditor
+                , auop, auopic
+                , gics.*
+            FROM (
+                SELECT *
+                FROM comp.funda
+                WHERE INDFMT = 'INDL'
+                    AND (DATAFMT = 'STD')
+                    AND (POPSRC = 'D')
+                    AND (CONSOL = 'C')
+                    AND (fyear >= 2000) AND (fyear < 2025)
+                    AND gvkey IS NOT NULL
+                    AND fyr IS NOT NULL
+                    AND at IS NOT NULL
+                    AND prcc_f IS NOT NULL
+                ) a
+            LEFT JOIN (SELECT gvkey, MIN(datadate) AS funda_start
+                    FROM comp.funda GROUP BY gvkey) ast
+                ON (a.gvkey = ast.gvkey)
+            LEFT JOIN ({gics}) AS gics
+                ON (a.gvkey = gics.gvkey_hgic
+                    AND gics.indfrom_hgic <= a.datadate
+                    AND a.datadate <= COALESCE(gics.indthru_hgic, current_date));
+            """
+
+        _logger.debug(f"Compustat query: {funda_query}")
+        with wrds_connection() as wrds:
+            df = wrds.read_sql(funda_query)
+        _logger.debug(f"Compustat result: {len(df)}")
+
+        for c in "gvkey_hgic indfrom_hgic indthru_hgic".split():
+            if c in df:
+                del df[c]
+
+        df = df.rename(
+                columns={
+                    "gsector_hgic": "gics_sector",
+                    "ggroup_hgic": "gics_group",
+                    "gind_hgic": "gics_industry",
+                    "gsubind_hgic": "gics_subindustry",
+                }
+            )
+        # Add GICS sector labels to gics_sector_name
+        df['gics_sector_name'] = df['gics_sector'].map({
+            10: 'Energy',
+            '10': 'Energy',
+            15: 'Materials',
+            '15': 'Materials',
+            20: 'Industrials',
+            '20': 'Industrials',
+            25: 'Consumer Discretionary',
+            '25': 'Consumer Discretionary',
+            30: 'Consumer Staples',
+            '30': 'Consumer Staples',
+            35: 'Health Care',
+            '35': 'Health Care',
+            40: 'Financials',
+            '40': 'Financials',
+            45: 'Information Technology',
+            '45': 'Information Technology',
+            50: 'Telecommunication Services',
+            '50': 'Telecommunication Services',
+            55: 'Utilities',
+            '55': 'Utilities',
+        })
+
+        df = (
+            df
+            .sort_values('at')
+            .assign(
+                firm_id=lambda df: df.groupby('gvkey', sort=False).ngroup(),
+            )
+            .drop(columns=["gvkey"])
+            .sort_values("firm_id fyear fiscal_year_end_month".split())
+            .drop_duplicates('firm_id fyear'.split())
+            .reset_index(drop=True)
+        )
+
+        cols = ['firm_id', 'tic', 'name', 'fyear', 'fiscal_year_end_month', 'age_days']
+        for c in df:
+            if c not in cols:
+                cols.append(c)
+        cols = [c for c in cols if c in df]
+
+        return df[cols]
